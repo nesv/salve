84683601073b759bd100a42f33cb91e688b258b8
Nick Saika <nicksaika@gmail.com>
2013-07-10 10:18:51 -0400

    Started laying out the struct for a Node.

---
+++ node.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/garyburd/redigo/redis"
+)
+
+/*
+A Node represents a partition in the cluster ring. A node consists of one
+Redis master, and N Redis slaves.
+*/
+type Node struct {
+	master redis.Conn
+	slaves []redis.Conn
+}
+
+/*
+NewNode creates a new "node" to be added to the cluster.
+*/
+func NewNode(addr string) (n *Node, err error) {
+	return
+}
+
+/*
+All requests to the node should be pass through the Run function.
+
+This function handles the round-robin load balancing on read operations, and
+ensuring any write operations are only performed on the master host, in the 
+node.
+*/
+func (n *Node) Run(c []byte) (r []byte, err error) {
+	return
+}
+
+func (n *Node) write([]byte) (r []byte, err error) {
+	return
+}
+
+func (n *Node) read([]byte) (r []byte, err error) {
+	return
+}
